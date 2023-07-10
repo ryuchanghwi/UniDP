02b758be7d2c0a2eff80174c43450df85b8ecd78
ryuchanghwi <ckdgnl10@naver.com>
2023-07-10 15:33:21 +0900

    테스트

--- Sources/UniDegisn/UniDegisn.swift
+++ Sources/UniDegisn/UniDegisn.swift
@@ -1,6 +1,30 @@
+
+import UIKit
+
+
 public struct UniDegisn {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    public class UniButton: UIButton {
+        public override init(frame: CGRect) {
+            super.init(frame: frame)
+            
+            applyCommonStyle()
+        }
+        public required init?(coder: NSCoder) {
+            super.init(coder: coder)
+            applyCommonStyle()
+            
+        }
+        private func applyCommonStyle() {
+            setTitleColor(.white, for: .normal)
+            titleLabel?.font = UIFont.boldSystemFont(ofSize: 16.0)
+            backgroundColor = .blue
+            layer.cornerRadius = 8.0
+            layer.borderWidth = 2.0
+            layer.borderColor = UIColor.white.cgColor
+        }
     }
 }
